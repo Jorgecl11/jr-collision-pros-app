db631eca395882bd23cecf26bb746300930cd27b
Jorge Chavez <jlchavez.cs@gmail.com>
2026-07-29 21:33:42 -0700

    Refactor Customer class into separate module

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-class Customer:
-
-    def __init__(self, name, phone_number, vin, license_plate):
-        self.name = name
-        self.phone_number = phone_number
-        self.vin = vin
-        self.license_plate = license_plate
-
-    def display_summary(self):
-        print()
-        print("=" * 25)
-        print("Customer Summary")
-        print(f"Name: {self.name.title()}")
-        print(f"Phone number: {self.phone_number}")
-        print(f"VIN: {self.vin.upper()}")
-        print(f"License Plate: {self.license_plate.upper()}")
-
-
-
+from customer import Customer
 
 def main():
     show_welcome()
