9d44ad85cf7d57e069e7508472a42f3282923cb0
Jorge Chavez <jlchavez.cs@gmail.com>
2026-09-09 18:43:29 -0700

    Test vehicle summary display

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -3,3 +3,10 @@ from vehicle import Vehicle
 def test_vehicle_stores_year():
     vehicle = Vehicle(1967, 'Ford','Mustang', 'CLASSICVIN67',  'CLASSIC67')
     assert vehicle.year == 1967
+
+def test_vehicle_displays_summary(capsys):
+    vehicle = Vehicle(2027, 'Lamborghini', 'Urus', 'HUMBLEURUS27', 'URUS27')
+    vehicle.display_summary()
+    captured = capsys.readouterr()
+
+    assert 'Year: 2027' in captured.out
